e67b84ef032b994c5dc5fa08ce032d02f7d94124
Marcin Dawidziuk <cinekdawidziuk@gmail.com>
2021-12-02 22:15:22 +0000

    Added table view

---
+++ IssueTracker.UI/components/IssueRow.vue
@@ -0,0 +1,80 @@
+﻿<template>
+  <div class="bg-white shadow rounded px-3 pt-2 pb-2 border border-white mt-2.5 cursor-move relative" draggable="false"
+       @contextmenu.prevent="isMenuOpened = !isMenuOpened">
+    <div v-if="isMenuOpened" class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+      <div class="py-1" role="none">
+        <a href="#" class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" tabindex="-1" id="menu-item-0"
+           @click="menuClicked('self-assign')"
+        >Assign to me</a>
+        <a href="#" class="text-red-700 block px-4 py-2 text-sm hover:bg-gray-100" role="menuitem" tabindex="-1" id="menu-item-1"
+           @click="menuClicked('delete')"
+        >Delete</a>
+      </div>
+    </div>
+    <div class="flex justify-between">
+      <p class="text-gray-700 font-semibold font-sans tracking-wide text-sm">
+        <span class="text-xs text-gray-600 mr-2">{{ reference }}</span>
+        <router-link :to="`/issues/${id}`">
+          {{ title }}
+        </router-link>
+      </p>
+      <div class="self-end">
+
+      </div>
+      <div class="flex">
+        <div v-for="label in labels" class="px-3 h-6 rounded-full text-xs font-semibold flex items-center bg-red-100 text-red-800">
+          <span class="w-2 h-2 rounded-full mr-1 bg-red-400"></span>
+          <span>{{ label.name }}</span>
+        </div>
+      <img v-if="avatar" :src="avatar" alt="Avatar" class="w-6 h-6 rounded-full ml-2" draggable="false">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {PropType, ref} from "vue";
+import {LabelDto} from "~/src/services/api.generated.clients";
+
+defineProps({
+  id: {
+    type: Number
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  tagName: {
+    type: String,
+    required: true
+  },
+  reference: {
+    type: String,
+    required: true
+  },
+  labels: {
+    type: Array as PropType<LabelDto[]>,
+    default: []
+  }
+})
+
+const emit = defineEmits(['self-assign', 'delete'])
+const menuClicked = function(name: 'self-assign' | 'delete'){
+  emit(name);
+  isMenuOpened.value = false
+}
+
+const isMenuOpened = ref(false)
+</script>
+
+<style scoped>
+
+</style>
--- IssueTracker.UI/pages/issues/index.vue
+++ IssueTracker.UI/pages/issues/index.vue
@@ -12,6 +12,24 @@
         Add issue
         </span>
     </router-link>
+    
+    <div class="bg-gray-50 rounded w-24 py-1 px-2 mt-2">
+      <button @click="isKanbanView = false" 
+              :class="!isKanbanView ? 'bg-gray-100 ring-2 ring-blue-300' : 'bg-gray-300'"
+              class=" rounded p-2 ">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
+        </svg>
+      </button>
+
+      <button @click="isKanbanView = true" 
+              class="rounded p-2 "
+        :class="isKanbanView ? 'bg-gray-100 ring-2 ring-blue-300' : 'bg-gray-300'">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17V7m0 10a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2h2a2 2 0 012 2m0 10a2 2 0 002 2h2a2 2 0 002-2M9 7a2 2 0 012-2h2a2 2 0 012 2m0 10V7m0 10a2 2 0 002 2h2a2 2 0 002-2V7a2 2 0 00-2-2h-2a2 2 0 00-2 2" />
+        </svg>
+      </button>
+    </div>
 
     <div class="flex -space-x-1 overflow-hidden mt-2 ml-3">
       <img v-for="user in userDropdowns" 
@@ -35,7 +53,7 @@
     </div>
 
 
-    <div class="min-h-screen flex overflow-x-scroll py-5">
+    <div v-if="isKanbanView" class="min-h-screen flex overflow-x-scroll py-5">
 
       <div v-for="column in issuesByColumn" class="bg-gray-100 rounded-lg px-3 py-3 column-width rounded mr-4 min-w-[300px] h-full">
         <p class="text-gray-700 font-semibold font-sans tracking-wide text-sm">{{ column.status.name }}</p>
@@ -80,9 +98,56 @@
               </div>
             </template>
           </draggable>
+        
           
       </div>
+    </div>
+    
+<!--    List view-->
+    <div v-else>
 
+      <div v-for="column in issuesByColumn" class="bg-gray-200 rounded-lg px-3 py-3 column-width rounded mr-4 min-w-[300px] h-full mb-5">
+        <p class="text-gray-700 font-semibold font-sans tracking-wide text-sm">{{ column.status.name }}</p>
+        <draggable
+            v-model="column.issues"
+            @change="onChange(column.status, $event)"
+            :animation="200"
+            item-key="id"
+            class="list-group min-h-[50px]"
+            group="tasks"
+            ghost-class="ghost"
+            @start="dragging = true"
+            @end="dragging = false"
+        >
+
+          <template #item="{ element }">
+            <div class="list-group-item">
+              <IssueRow
+                  @self-assign="selfAssignIssue(element)"
+                  :labels="element.labels"
+                  :key="element.id"
+                  :id="element.id"
+                  :title="element.title"
+                  :avatar="element.userName ? avatarUrl(element.userName) : null"
+                  :reference="element.reference"
+              />
+            </div>
+          </template>
+        </draggable>
+        <div class="hidden mt-4">
+          
+          <router-link :to="`/issues/add?projectId=${projectId}`" class="bg-gray-800 text-gray-50 p-2 rounded mt-5 w-max">
+            <div class="inline-block items-center align-middle">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+              </svg>
+            </div>
+            <span>
+          Add issue
+          </span>
+          </router-link>
+        </div>
+      </div>
 
 
     </div>
@@ -92,6 +157,7 @@
 <script setup lang="ts">
 
 import draggable from "vuedraggable";
+import IssueRow from "~/components/IssueRow.vue"
 import {avatarUrl, loadUserDropdowns, UserDropdownValue} from '~/src/services/utils';
 import {
   IssueDto,
@@ -124,6 +190,7 @@ const refreshIssuesByColumn = function (){
     issuesByColumn.value = i;
 }
 
+const isKanbanView = ref()
 
 const userDropdowns = ref<UserDropdownValue[]>([])
 const filterUser = ref<UserDropdownValue | null>(null)
